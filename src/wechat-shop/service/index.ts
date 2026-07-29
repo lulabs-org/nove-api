35d0a8852016415f5b177743ab06f180f8201485
杨仕明 <shiming.y@qq.com>
2026-07-29 11:05:46 +0800

    feat: implement wechat-shop module structure and export necessary DTOs, services, types, utils, and controllers

--- src/wechat-shop/service/index.ts
+++ src/wechat-shop/service/index.ts
@@ -1,1 +1,4 @@
+export * from './wechat-shop-client.service';
+export * from './wechat-shop-event.service';
 export * from './wechat-shop-order.service';
+export * from './wechat-shop-token.service';
