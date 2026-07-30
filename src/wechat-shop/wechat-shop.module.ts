732c68e551551283ec5d0151cf1bd0946c621c5b
杨仕明 <shiming.y@qq.com>
2026-07-30 11:35:16 +0800

    feat: integrate user creation and association for synced WeChat Shop orders

--- src/wechat-shop/wechat-shop.module.ts
+++ src/wechat-shop/wechat-shop.module.ts
@@ -14,11 +14,13 @@ import { WechatShopEventService } from './service/wechat-shop-event.service';
 import { WechatShopClientService } from './service/wechat-shop-client.service';
 import { WechatShopTokenService } from './service/wechat-shop-token.service';
 import { WechatShopProcessor } from './processor/wechat-shop.processor';
+import { UserModule } from '@/user/user.module';
 
 @Module({
   imports: [
     PrismaModule,
     HttpModule,
+    UserModule,
     ConfigModule.forFeature(wechatShopConfig),
     BullModule.registerQueue({ name: 'wechat-order-sync' }),
     BullBoardModule.forFeature({
