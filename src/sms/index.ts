7adf5adf9f4f3f01f151af659499f2bfb311f4f5
杨仕明 <shiming.y@qq.com>
2026-08-07 13:17:10 +0800

    refactor(sms): extract aliyun integration to abstract sms module

--- src/sms/index.ts
+++ src/sms/index.ts
@@ -3,11 +3,11 @@
  * @Date: 2025-09-23 06:15:34
  * @LastEditors: 杨仕明 shiming.y@qq.com
  * @LastEditTime: 2025-10-01 13:54:33
- * @FilePath: /lulab_backend/src/integrations/aliyun/index.ts
+ * @FilePath: /lulab_backend/src/sms/index.ts
  * @Description:
  *
  * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved.
  */
 
-export * from './aliyun.module';
-export * from './aliyun-sms.service';
+export * from './sms.module';
+export * from './sms.service';
